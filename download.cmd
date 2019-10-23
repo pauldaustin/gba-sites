4f654b16767844226daf173ae969f48966184a5a
Paul Austin <paul.austin@revolsys.com>
2019-10-22 17:47:15 -0700

    x

--- download.cmd
+++ download.cmd
@@ -1,10 +1,13 @@
 @echo off
-set PATH=e:\sw_nt\cygwin\bin\;C:\Apps\cygwin\bin;%PATH%
+set DIR=%~dp0
+set APPS_DIR=%DIR%\..\..\apps64
+set PATH=%APPS_DIR%\PortableGit;%PATH%
+
 SET VERSION=%1
 IF [%VERSION%] EQU [] set /p VERSION=Enter Version (e.g. 1.0.1-RELEASE): 
 IF [%VERSION%] EQU [] SET VERSION=%LATEST_VERSION%
-set DIR=%~dp0
+
 set DIR=%DIR:\=/%
-set DIR=/cygdrive/%DIR::=%
-bash -c "cd `cygpath %DIR%/..`; curl -O https://open.revolsys.com/artifactory/repo/ca/bc/gov/gba/gba-sites/%VERSION%/gba-sites-%VERSION%-bin.zip"
+set DIR=/%DIR::=%
+git-bash -c "cd `cygpath %DIR%/..`; curl -O https://open.revolsys.com/artifactory/gbasites-release-local/ca/bc/gov/gba/gba-sites/%VERSION%/gba-sites-%VERSION%-bin.zip"
 echo Downloaded %VERSION%
