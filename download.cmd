4b94d34c9afe4c1449d2560d138ea4309d4ed0b2
Paul Austin <paul.austin@revolsys.com>
2019-10-22 17:46:25 -0700

    x

--- download.cmd
+++ download.cmd
@@ -7,6 +7,7 @@ SET VERSION=%1
 IF [%VERSION%] EQU [] set /p VERSION=Enter Version (e.g. 1.0.1-RELEASE): 
 IF [%VERSION%] EQU [] SET VERSION=%LATEST_VERSION%
 
+set DIR=%DIR:\=/%
 set DIR=/%DIR::=%
 git-bash -c "cd `cygpath %DIR%/..`; curl -O https://open.revolsys.com/artifactory/gbasites-release-local/ca/bc/gov/gba/gba-sites/%VERSION%/gba-sites-%VERSION%-bin.zip"
 echo Downloaded %VERSION%
